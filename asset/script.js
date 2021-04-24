44ef5e44cb39f09ac38ec807bcd6824df9caf1fe
Rosellinda <64804606+Rosellinda@users.noreply.github.com>
2021-04-25 01:12:00 +0800

    comment out the loading css and js

--- asset/script.js
+++ asset/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-	// Users can skip the loading process if they want.
-	$('.skip').click(function() {
-		$('.overlay, body').addClass('loaded');
-	})
+// $(document).ready(function() {
+// 	// Users can skip the loading process if they want.
+// 	$('.skip').click(function() {
+// 		$('.overlay, body').addClass('loaded');
+// 	})
 	
-	// Will wait for everything on the page to load.
-	$(window).bind('load', function() {
-		$('.overlay, body').addClass('loaded');
-		setTimeout(function() {
-			$('.overlay').css({'display':'none'})
-		}, 2000)
-	});
+// 	// Will wait for everything on the page to load.
+// 	$(window).bind('load', function() {
+// 		$('.overlay, body').addClass('loaded');
+// 		setTimeout(function() {
+// 			$('.overlay').css({'display':'none'})
+// 		}, 2000)
+// 	});
 	
-	// Will remove overlay after 1min for users cannnot load properly.
-	setTimeout(function() {
-		$('.overlay, body').addClass('loaded');
-	}, 30000);
-})
+// 	// Will remove overlay after 1min for users cannnot load properly.
+// 	setTimeout(function() {
+// 		$('.overlay, body').addClass('loaded');
+// 	}, 30000);
+// })
 
 const header = document.getElementById('header');
 const toggle = document.getElementById('toggle');
